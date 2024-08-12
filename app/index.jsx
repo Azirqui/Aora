0e0079534c23552de187d9e2709bed111ace0ade
Azirqui <mr.noman0095@gmail.com>
2024-08-12 10:07:54 +0500

    Continue Working...

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import {Link} from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
 
-const RootLayout = () => {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Aora</Text>
-      <Text>Yomoloco Scene</Text>
+    <View>
+      <Text>Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style= {{color: 'blue'}}>Login</Link>
     </View>
   )
 }
 
-export default RootLayout
+export default App
 
-const styles = StyleSheet.create({
-  container: {
-    display: 'flex',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-})
+const styles = StyleSheet.create({})
